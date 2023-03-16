acbb7076e2b15662abd7f4b23425fb76a86009a6
jeanheberth <jeanheberth19@gmail.com>
2023-03-16 16:25:54 -0300

    init project

---
+++ src/main/java/br/com/jeanheberth/retornojson/RetornoJsonApplication.java
@@ -0,0 +1,13 @@
+package br.com.jeanheberth.retornojson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetornoJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetornoJsonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/jeanheberth/retornojson/RetornoJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jeanheberth.retornojson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetornoJsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

42a2f4f982ea4e13d2a87d8f93c3dcef040414c7
jeanheberth <jeanheberth19@gmail.com>
2023-03-16 21:57:07 -0300

    foi atualizado o método consultar_usuario

--- src/test/java/br/com/jeanheberth/retornojson/page/PaginaTest.java
+++ src/test/java/br/com/jeanheberth/retornojson/page/PaginaTest.java
@@ -4,16 +4,12 @@ import br.com.jeanheberth.retornojson.constantes.Constantes;
 import br.com.jeanheberth.retornojson.utils.base.BaseTeste;
 import io.restassured.http.ContentType;
 import io.restassured.module.jsv.JsonSchemaValidator;
-import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
-import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
-import static io.restassured.RestAssured.basePath;
 import static org.hamcrest.Matchers.*;
 
-public class testPagina extends BaseTeste {
+class PaginaTest extends BaseTeste {
 
     @Test
     void testeRetornarPagina() {
